6c19972d10dd3f69dcca6b5cc1ef2bd207486447
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-06 14:38:21 +0900

    Add files via upload

---
+++ fk3daw.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/LineageW/20241104/bg_01.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
